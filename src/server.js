6356761daf51ac7b8c03ea39d87c32894e504592
Marius <marius@example.com>
2022-08-26 12:03:42 +0300

    /api/posts

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const morgan = require('morgan');
 // eslint-disable-next-line no-unused-vars
 const colors = require('colors');
+const mysql = require('mysql2/promise');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -24,6 +25,25 @@ app.use(morgan('dev'));
 // Routes
 app.get('/', (req, res) => {
   res.json({ msg: 'Server online' });
+});
+
+app.get('/api/posts', async (req, res) => {
+  try {
+    // 1. prisijungti prie db
+    const connection = await mysql.createConnection(dbConfig);
+    console.log('Conected to DB'.bgGreen.bold);
+    // 2. atlikti veiksma
+    const sql = 'SELECT * FROM posts WHERE id = 2';
+    const [rows, fields] = await connection.query(sql);
+    console.log('rows ===', rows);
+    res.json(rows);
+    // 3.uzdaryti prisijungima
+    connection.end();
+  } catch (error) {
+    console.log('Error Conecting to DB'.bgRed.bold, error);
+    // 4. gaudyti klaidas
+    res.status(500).json({ msg: 'something went worng' });
+  }
 });
 
 app.use((req, res) => {
