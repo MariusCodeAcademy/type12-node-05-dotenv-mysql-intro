c9b37741abd1ed651c374c5b87e4d14c3916f376
Marius <marius@example.com>
2022-08-26 09:46:34 +0300

    dotenv

--- src/server.js
+++ src/server.js
@@ -1,13 +1,16 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 // eslint-disable-next-line no-unused-vars
 const colors = require('colors');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(morgan('dev'));
+
+// connect(process.env.LOGIN, process.env.PASS)
 
 // Routes
 app.get('/', (req, res) => {
@@ -19,5 +22,7 @@ app.use((req, res) => {
     msg: 'Not found',
   });
 });
+
+console.log('process.env.LOGIN ===', process.env.LOGIN);
 
 app.listen(port, () => console.log(`server is running on port ${port}`.bgYellow.bold));
