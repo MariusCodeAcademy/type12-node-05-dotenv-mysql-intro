a7c14764945e52b0563899afc3f30e88bc1525e3
Marius <marius@example.com>
2022-08-26 09:26:30 +0300

    server online

--- src/server.js
+++ src/server.js
@@ -1,1 +1,23 @@
-console.log('aha');
+const express = require('express');
+const morgan = require('morgan');
+// eslint-disable-next-line no-unused-vars
+const colors = require('colors');
+
+const app = express();
+const port = 3000;
+
+// Middleware
+app.use(morgan('dev'));
+
+// Routes
+app.get('/', (req, res) => {
+  res.json({ msg: 'Server online' });
+});
+
+app.use((req, res) => {
+  res.status(404).json({
+    msg: 'Not found',
+  });
+});
+
+app.listen(port, () => console.log(`server is running on port ${port}`.bgYellow.bold));
