97ae785afff654d9dff0ed2f946baa742545a825
Marius <marius@example.com>
2022-08-29 08:40:15 +0300

    export

---
+++ posts_export_2022-08-29_083636.sql
@@ -0,0 +1,5 @@
+insert into `posts` (`author`, `body`, `category`, `createdAt`, `id`) values ('James Bond', 'Post of James bond about 007', 'movies', '2022-08-29 08:29:42', 1);
+insert into `posts` (`author`, `body`, `category`, `createdAt`, `id`) values ('Jill Crown', 'post of Jill about Hill', 'books', '2022-08-29 08:29:49', 2);
+insert into `posts` (`author`, `body`, `category`, `createdAt`, `id`) values ('Serbentautas Bordiuras', 'All about serbentai', 'comedy', '2022-08-29 08:29:56', 3);
+insert into `posts` (`author`, `body`, `category`, `createdAt`, `id`) values ('James Bond ', '008 is the next movie', 'movies', '2022-08-29 08:32:11', 4);
+insert into `posts` (`author`, `body`, `category`, `createdAt`, `id`) values ('Serbentautas Bordiuras', 'Arbuzu kainu suolis i vasaros pabaiga. ', 'comedy', '2022-08-29 08:32:30', 5);
--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 
 // Config
 const dbConfig = {
-  host: 'localhost',
+  host: process.env.DB_HOST,
   user: 'root',
   password: '', // pas ka mamp 'root'
   port: '3306', // mamp 8889
@@ -27,13 +27,13 @@ app.get('/', (req, res) => {
   res.json({ msg: 'Server online' });
 });
 
-app.get('/api/posts', async (req, res) => {
+app.get('/api/posts/', async (req, res) => {
   try {
     // 1. prisijungti prie db
     const connection = await mysql.createConnection(dbConfig);
     console.log('Conected to DB'.bgGreen.bold);
     // 2. atlikti veiksma
-    const sql = 'SELECT * FROM posts WHERE id = 2';
+    const sql = 'SELECT * FROM posts';
     const [rows, fields] = await connection.query(sql);
     console.log('rows ===', rows);
     res.json(rows);
@@ -46,6 +46,10 @@ app.get('/api/posts', async (req, res) => {
   }
 });
 
+// GET /api/posts/order/desc - gauti postus isrikiuotus pagas varda
+// const sql = 'SELECT * FROM posts ORDER BY author DESC';
+
+// GET /api/posts/order/desc - desc dalis butu dinamine galimos reiksmes asc|desc
 app.use((req, res) => {
   res.status(404).json({
     msg: 'Not found',
